6647a60bc2a9864519e9a00de444a441ad7f23d4
Brian Christian <brchristian@gmail.com>
2016-12-05 16:03:01 -0800

    version bumps for rails5 support

--- lib/casein.rb
+++ lib/casein.rb
@@ -1,10 +1,10 @@
-if defined?(Rails) && Rails::VERSION::MAJOR == 4
+if defined?(Rails) && Rails::VERSION::MAJOR >= 4
 	require 'casein/engine'
 	require 'casein/version'
 	require 'will_paginate'
 	require 'authlogic'
 else
 	puts("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-	puts("!!! WARNING !!! This version of Casein requires Rails 4.x !!!")
+	puts("!!! WARNING! This version of Casein requires Rails >= 4.x !!!")
 	puts("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 end
