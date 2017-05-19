e1af9d9a0f45ac6d6d2f672a56f4dc96551a446b
Russell Quinn <mail@russellquinn.com>
2017-05-18 19:06:24 -0700

    Bump version to 5.3.0

--- lib/casein/version.rb
+++ lib/casein/version.rb
@@ -1,4 +1,4 @@
 module Casein
-  VERSION_HASH = { major: 5, minor: 2, patch: 0, build: 0 }
+  VERSION_HASH = { major: 5, minor: 3, patch: 0, build: 0 }
   VERSION = VERSION_HASH.values.join(".")
 end
