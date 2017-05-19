da29b2c028d922ae31d5b7433fef8f1dc66b57fc
Russell Quinn <mail@russellquinn.com>
2017-05-18 20:13:04 -0700

    Updated the migration template used for casein:scaffold's --create-model-and-migration option to include Rails version number and improved formatting

--- lib/generators/casein/scaffold/templates/migration.rb
+++ lib/generators/casein/scaffold/templates/migration.rb
@@ -1,13 +1,9 @@
-class Create<%= class_name.pluralize %> < ActiveRecord::Migration
-  def self.up
+class Create<%= class_name.pluralize %> < ActiveRecord::Migration[<%= Rails::VERSION::MAJOR %>.<%= Rails::VERSION::MINOR %>]
+  def change
     create_table :<%= table_name %> do |t|
-      <% attributes.each do |attribute| %>t.<%= attribute.type %> :<%= attribute.name %>
+<% attributes.each do |attribute| %>      t.<%= attribute.type %> :<%= attribute.name %>
 <% end %>
       t.timestamps
     end
-  end
-
-  def self.down
-    drop_table :<%= table_name %>
   end
 end
